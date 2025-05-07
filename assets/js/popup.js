dedf1a4eb6a05ab3a37abdefea3a6a38dd9a1b9e
Rubens Freitas <rubens.suporte@gmail.com>
2025-05-07 02:04:55 -0300

    muted

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,25 +1,33 @@
-new Swiper('.testimonials-slider', {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false
-    },
-    slidesPerView: 'auto',
-    pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    },
-    breakpoints: {
-      320: {
-        slidesPerView: 1,
-        spaceBetween: 20
-      },
+var videos = document.querySelectorAll('video');
 
-      1200: {
-        slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
-  });
+// Clique ou toque para tocar/parar
+videos.forEach(video => {
+    const togglePlay = () => {
+        video.classList.toggle('active');
+
+        if (video.paused) {
+            video.play();
+        } else {
+            video.pause();
+            video.currentTime = 0;
+        }
+    };
+
+    video.addEventListener('click', togglePlay);
+    video.addEventListener('touchstart', togglePlay); // suporte para mobile
+});
+
+// Parar vídeo ao sair da tela
+let observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            entry.target.pause();
+            entry.target.currentTime = 0;
+            entry.target.classList.remove('active');
+        }
+    });
+}, {
+    threshold: 0.25
+});
+
+videos.forEach(video => observer.observe(video));
